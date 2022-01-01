315677a7108d6f6a3e9742767c1b1465e45603ea
Owen Roth <69156111+oroth8@users.noreply.github.com>
2022-01-01 14:07:51 -0600

    added show inline editing

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -2,10 +2,12 @@
 
 class DiscussionsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_discussion, only: %i[edit update destroy]
+  before_action :set_discussion, only: %i[show edit update destroy]
   def index
     @discussions = Discussion.all
   end
+
+  def show; end
 
   def new
     @discussion = Discussion.new
@@ -15,7 +17,7 @@ class DiscussionsController < ApplicationController
     @discussion = Discussion.new(discussion_params)
     respond_to do |format|
       if @discussion.save
-        format.html { redirect_to discussions_path, notice: 'Discussion Created!' }
+        format.html { redirect_to @discussion, notice: 'Discussion Created!' }
       else
         format.html { render :new, status: :unprocessable_entity }
       end
@@ -27,7 +29,7 @@ class DiscussionsController < ApplicationController
   def update
     respond_to do |format|
       if @discussion.update(discussion_params)
-        format.html { redirect_to discussions_path, notice: 'Discussion Updated!' }
+        format.html { redirect_to @discussion, notice: 'Discussion Updated!' }
       else
         format.html { render :edit, status: :unprocessable_entity }
       end
