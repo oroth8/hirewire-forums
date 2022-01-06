6c9d82089f7cec826ce7e0eefb1688a91a54752b
Owen Roth <69156111+oroth8@users.noreply.github.com>
2022-01-06 17:49:31 -0600

    added category filtering

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   root to: 'main#index'
   resources :discussions, only: %i[index show new create edit update destroy] do
     resources :posts, only: %i[create show edit update destroy], module: :discussions
+
+    collection do
+      get 'category/:id', to: 'categories/discussions#index', as: :category
+    end
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
