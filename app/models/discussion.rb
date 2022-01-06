deb20280723d5ce0b4672dad8bcd3eb4aad147ee
Owen Roth <69156111+oroth8@users.noreply.github.com>
2022-01-06 17:03:25 -0600

    added categories partial and hotwire

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -4,6 +4,8 @@ class Discussion < ApplicationRecord
   belongs_to :user, default: -> { Current.user }
   belongs_to :category, counter_cache: true, touch: true
 
+  delegate :name, prefix: :category, to: :category, allow_nil: true
+
   validates :name, presence: true, uniqueness: true
 
   has_many :posts, dependent: :destroy
